afcb8b82427b7d21514286783614ccb2b80a9d81
portatil-jose <info@ramosandrades.com>
2022-02-28 13:04:13 +0100

    Initial Commit, web service with TestController helloworld

---
+++ src/main/java/com/example/restservice/TestController.java
@@ -0,0 +1,13 @@
+package com.example.restservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/helloworld")
+    public String helloworld(){
+        return "Hello World";
+    }
+}
