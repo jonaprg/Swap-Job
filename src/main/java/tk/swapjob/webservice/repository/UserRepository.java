545cc9b7e2929cae6f4d3d2a87ddedb98b50531e
1565479 - Jose A. Ramos Andrades <joseantonio.ramosa@autonoma.cat>
2022-04-25 08:59:21 +0200

    Added docker-compose for local development

--- src/main/java/tk/swapjob/webservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package tk.swapjob.webservice.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import tk.swapjob.webservice.model.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findUserByEmail(String email);
-    boolean existsUserByEmail(String email);
-}
